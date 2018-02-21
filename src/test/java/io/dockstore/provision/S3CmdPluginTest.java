c21fc90c18ba14c2eed53d0cd061b149478699b2
garyluu <garylau.work@gmail.com>
2018-02-21 13:52:48 -0500

    Test for s3cmd install directory

--- src/test/java/io/dockstore/provision/S3CmdPluginTest.java
+++ src/test/java/io/dockstore/provision/S3CmdPluginTest.java
@@ -1,10 +1,12 @@
 package io.dockstore.provision;
 
 import java.io.File;
-import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,8 +19,9 @@ import static org.junit.Assert.assertTrue;
  * @since 07/03/17
  */
 public class S3CmdPluginTest {
-    S3CmdPlugin.S3CmdProvision icgcGetProvision;
     final String resourcesDirectory = new File("src/test/resources").getAbsolutePath();
+    S3CmdPlugin.S3CmdProvision icgcGetProvision;
+
     @Before
     public void before() throws Exception {
         icgcGetProvision = new S3CmdPlugin.S3CmdProvision();
@@ -27,8 +30,16 @@ public class S3CmdPluginTest {
 
     private void setConfiguration() throws Exception {
         HashMap hm = new HashMap();
-        hm.put("client", "/home/travis/.local/bin/s3cmd");
-//        hm.put("client", "/usr/local/bin/s3cmd");
+        List<String> s3cmdLocations = Arrays.asList("/usr/local/bin/s3cmd", "/home/travis/.local/bin/s3cmd");
+        Optional<String> first = s3cmdLocations.stream().filter(s3cmdLocation -> {
+            File file = new File(s3cmdLocation);
+            return file.exists();
+        }).findFirst();
+        if (first.isPresent()) {
+            hm.put("client", first.get());
+        } else {
+            throw new RuntimeException("No s3cmd installed");
+        }
         hm.put("config-file-location", resourcesDirectory + "/.s3cfg");
         hm.put("verbosity", "Minimal");
         icgcGetProvision.setConfiguration(hm);
@@ -37,10 +48,10 @@ public class S3CmdPluginTest {
     private void download(String source, String destination) {
         String sourcePath = source;
         File f = new File(destination);
-        assertFalse(f.getAbsolutePath() + " already exists.",f.exists());
+        assertFalse(f.getAbsolutePath() + " already exists.", f.exists());
         Path destinationPath = Paths.get(f.getAbsolutePath());
         assertTrue(icgcGetProvision.downloadFrom(sourcePath, destinationPath));
-        assertTrue(f.getAbsolutePath() + " not downloaded." , f.exists());
+        assertTrue(f.getAbsolutePath() + " not downloaded.", f.exists());
     }
 
     /**
@@ -52,7 +63,7 @@ public class S3CmdPluginTest {
         Path sourceFile = Paths.get(resourcesDirectory + "/inputFilesDirectory/file.txt");
         icgcGetProvision.uploadTo(destPath, sourceFile, null);
         String source = "s3cmd://test-bucket1/file2.txt";
-        String destination = resourcesDirectory +"/outputFilesDirectory1/file3.txt";
+        String destination = resourcesDirectory + "/outputFilesDirectory1/file3.txt";
         download(source, destination);
     }
 
@@ -65,7 +76,7 @@ public class S3CmdPluginTest {
         Path sourceFile = Paths.get(resourcesDirectory + "/inputFilesDirectory/file.txt");
         icgcGetProvision.uploadTo(destPath, sourceFile, null);
         String source = "s3cmd://test-bucket2/file.txt";
-        String destination = resourcesDirectory +"/outputFilesDirectory2/file2.txt";
+        String destination = resourcesDirectory + "/outputFilesDirectory2/file2.txt";
         download(source, destination);
     }
 
@@ -78,10 +89,10 @@ public class S3CmdPluginTest {
         Path sourceFile = Paths.get(resourcesDirectory + "/inputFilesDirectory");
         icgcGetProvision.uploadTo(destPath, sourceFile, null);
         String source = "s3cmd://test-bucket3/inputFilesDirectory/file.txt";
-        String destination = resourcesDirectory +"/outputFilesDirectory3/file.txt";
+        String destination = resourcesDirectory + "/outputFilesDirectory3/file.txt";
         download(source, destination);
         source = "s3cmd://test-bucket3/inputFilesDirectory/file2.txt";
-        destination = resourcesDirectory +"/outputFilesDirectory3/file2.txt";
+        destination = resourcesDirectory + "/outputFilesDirectory3/file2.txt";
         download(source, destination);
     }
 }
